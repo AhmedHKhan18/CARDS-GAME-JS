24efdac282fb5b778ff09b0558183ab82b1c8307
AhmedHKhan18 <ahmed.18hassan.18@gmail.com>
2024-04-27 13:52:09 +0500

    Cards Game

---
+++ app.js
@@ -0,0 +1,20 @@
+function flip(event){
+    event.src = "/assets/Queen.jpg";
+}
+function unflip(event){
+    event.src = "/assets/backsidecard.jpg";
+}
+function flip2(event2){
+    event2.src = "/assets/King.png";
+    console.log(event2);
+}
+function unflip2(event2){
+    event2.src = "/assets/backsidecard.jpg";
+    console.log(event2);
+}
+function flip3(event3){
+    event3.src = "/assets/Jack.png";
+}
+function unflip3(event3){
+    event3.src = "/assets/backsidecard.jpg";
+}
